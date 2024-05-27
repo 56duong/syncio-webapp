43ec0ff3915293d09dc481b022aad3b1c5c46066
Vanhuyne <106498928+Vanhuyne@users.noreply.github.com>
2024-05-27 11:09:06 +0700

    add create post

--- frontend/src/app/features/user/user-home/left-menu/left-menu.component.ts
+++ frontend/src/app/features/user/user-home/left-menu/left-menu.component.ts
@@ -1,39 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CreatePostComponent } from '../create-post/create-post.component';
 
 @Component({
   selector: 'app-left-menu',
   templateUrl: './left-menu.component.html',
-  styleUrls: ['./left-menu.component.scss']
+  styleUrls: ['./left-menu.component.scss'],
 })
-
 export class LeftMenuComponent {
+  @ViewChild(CreatePostComponent) createPostComponent: any;
   visible: boolean = false;
 
   menus: any[] = [
     {
       label: 'Home',
       icon: 'pi pi-home',
-      routerLink: '/'
+      routerLink: '/',
     },
     {
       label: 'Search',
       icon: 'pi pi-search',
-      routerLink: '/search'
+      routerLink: '/search',
     },
     {
       label: 'Messages',
       icon: 'pi pi-comments',
-      routerLink: 'messages'
+      routerLink: 'messages',
     },
     {
       label: 'Profile',
       icon: 'pi pi-user',
-      routerLink: 'profile'
-    }
+      routerLink: 'profile',
+    },
   ];
 
-  showCreatePostDialog() {
-    this.visible = true;
+  onCreateClick() {
+    this.createPostComponent.showDialog();
   }
-
 }
