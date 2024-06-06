d594b977de887f9a7afa788e3b01d3209aac3a53
ndqThuan <thuanndq@gmail.com>
2024-06-06 12:08:10 +0700

    Added getting posts created by a user function

--- backend/src/main/java/online/syncio/backend/post/PostRepository.java
+++ backend/src/main/java/online/syncio/backend/post/PostRepository.java
@@ -3,10 +3,12 @@ package online.syncio.backend.post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
 
-    long countByCreatedBy_Id (UUID id);
+
+    List<Post> findByUserID (UUID id);
 }
