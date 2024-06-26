10616797bbbd3e2e78f673d609ed410c66a2f9c6
56duong <nguyenduong07122003@gmail.com>
2024-06-26 15:13:17 +0700

    Revert commit 8fcfe8f

--- backend/src/main/java/online/syncio/backend/post/PostService.java
+++ backend/src/main/java/online/syncio/backend/post/PostService.java
@@ -241,16 +241,15 @@ public class PostService {
     @Transactional
     public ResponseEntity<?> toggleLike (UUID postId, UUID userId) {
         try {
-
-
             Post post = postRepository.findById(postId)
                                       .orElseThrow(() -> new NotFoundException(Post.class, "id", postId.toString()));
 
             User user = userRepository.findById(userId)
                                       .orElseThrow(() -> new NotFoundException(User.class, "id", userId.toString()));
 
-            Optional<Like> existingLike = likeRepository.findLikeByPostAndUser(postId, userId);
-
+            Optional<Like> existingLike = post.getLikes().stream()
+                                              .filter(like -> like.getUser().equals(user))
+                                              .findFirst();
 
             if (existingLike.isPresent()) {
                 post.getLikes().remove(existingLike.get());
