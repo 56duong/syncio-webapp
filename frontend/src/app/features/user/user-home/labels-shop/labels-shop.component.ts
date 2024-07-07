e623c5768f0759d0722b2c36f557fe97afbb37c6
sanhnguyen <@CoanhHeo>
2024-07-07 19:57:55 +0700

    feat: Update labels shop component UI and functionality
    Refactor the labels shop component HTML to improve UI and functionality:
    - Updated the search input field to be aligned with the sort dropdown.
    - Removed the export file button and replaced it with a purchase history button.
    - Added sorting functionality to the labels data view based on price.

--- frontend/src/app/features/user/user-home/labels-shop/labels-shop.component.ts
+++ frontend/src/app/features/user/user-home/labels-shop/labels-shop.component.ts
@@ -10,68 +10,93 @@ import { LabelResponse } from 'src/app/core/interfaces/label-response';
 import { ToastService } from 'src/app/core/services/toast.service';
 import { BillingService } from 'src/app/core/services/billing.service';
 import { Billing } from 'src/app/core/interfaces/billing';
-
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-labels-shop',
   templateUrl: './labels-shop.component.html',
-  styleUrls: ['./labels-shop.component.scss']
+  styleUrls: ['./labels-shop.component.scss'],
 })
-
 export class LabelsShopComponent {
-    labelDialog: boolean = false;
-    billOfUserDialog: boolean = false;
-    submitted: boolean = false;
-    labels!: LabelResponse[];
-    label!: LabelResponse;
-    vnpay!: VNPay;
-    type?: string;
-    statuses?: any[];
-    dateNow: any = null;
-    user?: UserResponse | null = this.userService.getUserResponseFromLocalStorage();
-    bills!: Billing[];
+  labelDialog: boolean = false;
+  billOfUserDialog: boolean = false;
+  submitted: boolean = false;
+  labels!: LabelResponse[];
+  label!: LabelResponse;
+  vnpay!: VNPay;
+  type?: string;
+  statuses?: any[];
+  dateNow: any = null;
+  user?: UserResponse | null =
+    this.userService.getUserResponseFromLocalStorage();
+  bills!: Billing[];
+  sortOptions!: SelectItem[];
+  sortOrder!: number;
+  sortField!: string;
+  sortKey!: string;
 
-    constructor(
-      private labelService: LabelService,
-      private userService: UserService,
-      private toastService: ToastService,
-      private paymentService: PaymentService,
-      private billingService: BillingService
-    ) {}
+  constructor(
+    private labelService: LabelService,
+    private userService: UserService,
+    private toastService: ToastService,
+    private paymentService: PaymentService,
+    private billingService: BillingService
+  ) {}
 
-    ngOnInit() {
-      if (this.user?.id){
-        this.labelService.getLabelsWithPurchaseStatus(this.user?.id).subscribe({
-          next: (data) => {
-              console.log(data);
-              this.labels = data;
-              this.dateNow = Date.now();
-              this.labels.forEach((label) => label.type = label.labelURL?.split('.').pop()?.toLocaleUpperCase());
-          },
-          error: (error) => {
-              console.error('Error fetching labels', error);
-          },
-        });
-      }
+  ngOnInit() {
+    if (this.user?.id) {
+      this.labelService.getLabelsWithPurchaseStatus(this.user?.id).subscribe({
+        next: (data) => {
+          console.log(data);
+          this.labels = data;
+          this.dateNow = Date.now();
+          this.labels.forEach(
+            (label) =>
+              (label.type = label.labelURL
+                ?.split('.')
+                .pop()
+                ?.toLocaleUpperCase())
+          );
+        },
+        error: (error) => {
+          console.error('Error fetching labels', error);
+        },
+      });
     }
+    this.sortKey = 'price';
+    this.sortOptions = [
+      { label: 'Price High to Low', value: '!price' },
+      { label: 'Price Low to High', value: 'price' },
+    ];
+  }
 
+  onSortChange(event: any) {
+    let value = event.value;
+
+    if (value.indexOf('!') === 0) {
+      this.sortOrder = -1;
+      this.sortField = value.substring(1, value.length);
+    } else {
+      this.sortOrder = 1;
+      this.sortField = value;
+    }
+  }
   hideDialog() {
-      //this.selectedLabels = []; // xoá label đã chọn -> khi mở dialog lên sẽ không hiển thị label đã chọn
-      this.labelDialog = false;
-      this.submitted = false;
+    //this.selectedLabels = []; // xoá label đã chọn -> khi mở dialog lên sẽ không hiển thị label đã chọn
+    this.labelDialog = false;
+    this.submitted = false;
   }
 
   buyNow(label: Label) {
     this.submitted = true;
 
     if (label.id) {
-      let params = new HttpParams()
-      .set('labelID', label.id)
+      let params = new HttpParams().set('labelID', label.id);
 
-      console.log("params: " + params.toString());
+      console.log('params: ' + params.toString());
 
       this.paymentService.createVNPayPayment(params).subscribe({
-        next: (data)  => {
+        next: (data) => {
           this.vnpay = data;
           console.log(data);
 
@@ -79,14 +104,13 @@ export class LabelsShopComponent {
             window.location.href = this.vnpay.paymentURL;
           }
         },
-        
+
         error: (error) => {
           this.toastService.showError('Error', error.error.message);
-        }
+        },
       });
     }
   }
-
 
   gift(label: Label) {
     this.label = { ...label };
@@ -96,21 +120,23 @@ export class LabelsShopComponent {
   sendGift() {
     this.submitted = true;
 
-    if (this.label.owner == null || this.label.owner == "") {
-      this.toastService.showError('Error','Please enter username you want to send a gift');
+    if (this.label.owner == null || this.label.owner == '') {
+      this.toastService.showError(
+        'Error',
+        'Please enter username you want to send a gift'
+      );
       return;
     }
 
     if (this.label.id) {
-      let params = new HttpParams()
-      .set('labelID', this.label.id)
+      let params = new HttpParams().set('labelID', this.label.id);
 
       if (this.label.owner) {
         params = params.set('owner', this.label.owner);
       }
 
       this.paymentService.createVNPayPayment(params).subscribe({
-        next: (data)  => {
+        next: (data) => {
           this.vnpay = data;
           console.log(data);
 
@@ -118,10 +144,10 @@ export class LabelsShopComponent {
             window.location.href = this.vnpay.paymentURL;
           }
         },
-        
+
         error: (error) => {
           this.toastService.showError('Error', error.error.message);
-        }
+        },
       });
     }
   }
@@ -141,15 +167,14 @@ export class LabelsShopComponent {
 
   getSeverity(status: string) {
     switch (status) {
-        case 'SUCCESS':
-            return 'success';
-        case 'PROCESSING':
-            return 'warning';
-        case 'FAILED':
-            return 'danger';
-        default:
-            return 'info';
+      case 'SUCCESS':
+        return 'success';
+      case 'PROCESSING':
+        return 'warning';
+      case 'FAILED':
+        return 'danger';
+      default:
+        return 'info';
     }
-}
-
+  }
 }
