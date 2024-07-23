7124cc694e915f568cb93d7a691167604a8547de
chuthanh <mamnonc23@gmail.com>
2024-07-23 20:54:12 +0700

    23072024

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl:
-    'http://k8s-default-awsingre-9aa4c55379-27141432.ap-southeast-1.elb.amazonaws.com/backend/',
+  apiUrl: 'http://localhost:8080/',
   apiPythonUrl: 'http://localhost:5000/',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://k8s-default-awsingre-9aa4c55379-27141432.ap-southeast-1.elb.amazonaws.com/backend/',
+  apiUrl: 'http://localhost:8080/',
   apiPythonUrl: 'http://localhost:5000/',
 };
