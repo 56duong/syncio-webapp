65ae6d9fcd5916fb0ac693820538a533f9c5aa4f
56duong <nguyenduong07122003@gmail.com>
2024-08-18 23:26:22 +0700

    fix: download link in about us page

--- frontend/src/environments/environment.android.ts
+++ frontend/src/environments/environment.android.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/',
+  production: true,
+  apiUrl: 'https://syncio.space/',
   apiPythonUrl: 'http://localhost:5000/',
   android: true,
   windows: false,
--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/',
   apiPythonUrl: 'http://localhost:5000/',
-  android: true,
+  android: false,
   windows: false,
   githubToken: 'YOUR_ACTUAL_GITHUB_TOKEN'
 };
